95a5863a1418ae10af3968f9f346bb5f7c0bc6fe
panzhengguang <zhengguangpan@hotmail.com>
2015-11-30 15:57:48 +0800

    公共头文件

---
+++ boost_demo/std.hpp
@@ -0,0 +1,23 @@
+/*
+ * std.hpp
+ *
+ *  Created on: 2015年11月30日
+ *      Author: panzg
+ */
+
+#ifndef STD_HPP_
+#define STD_HPP_
+
+
+#include <cassert>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <set>
+#include <map>
+#include <algorithm>
+using namespace std;
+
+
+
+#endif /* STD_HPP_ */
