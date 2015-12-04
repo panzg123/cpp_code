17068fd3a2e0b96d6d71429da00b05067ac2054c
panzhenguang <panzg123@qq.com>
2015-12-04 10:19:56 +0800

    配置文件类的测试

---
+++ basic/conf_unit_test.cpp
@@ -0,0 +1,22 @@
+/*
+ * conf_unit_test.cpp conf_utils的测试函数
+ *
+ *  Created on: 2015年12月4日
+ *      Author: panzg
+ */
+
+#include<iostream>
+#include"../src/conf_utils.h"
+using namespace std;
+
+
+#if 0
+int main()
+{
+	MyConf conf("docs/conf_Test.txt");
+	std::cout << "program_name: " << conf.get("program_name") << std::endl;
+	std::cout << "working_directory: " << conf.get("working_directory")<< std::endl;
+
+}
+
+#endif
